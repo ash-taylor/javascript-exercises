44faa4caed8e3eb51e3172e5e60209f257d9afd1
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-02 16:59:57 +0000

    Solves task 02 - function to repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num === 0 || string === "") {
+    return "";
+  }
+  else if (num < 0) {
+    return "ERROR";
+  }
+  else {
+    let result = string;
+    for (let i = 0; i < num - 1; i++) {
+      result += string;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
