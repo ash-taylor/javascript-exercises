41c9724e75959f51aeb6d921cc22148bdcb15765
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-03 18:43:32 +0000

    Solves task 03 with simple for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  if (string === "") {
+    return "";
+  }
+  else {
+    n = string.length;
+    let newString = string[n - 1]
+  
+    for (let i = (string.length - 2); i >= 0; i--) {
+      newString += string[i];
+    }
+    return newString;
+  }
 };
 
 // Do not edit below this line
