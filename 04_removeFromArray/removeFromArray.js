0545afc05e2f9d607a8c6c81db9b2b26a1c687d0
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-04 19:42:55 +0000

    Completes task 04 using arguments obj and array methods

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
 
+  let newArray = Array.from(arguments[0]);
+
+  for (let i = 1; i < arguments.length; i++) {
+    if (newArray.indexOf(arguments[i]) === -1) {
+      continue;
+    }
+    else {
+      newArray.splice(newArray.indexOf(arguments[i]), 1);
+    }  
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
