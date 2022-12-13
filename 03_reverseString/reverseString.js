16a5298475f0a4df4484227ed0b118462e4c5852
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-13 15:57:40 +0000

    Added commented alternative using methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,16 +1,23 @@
-const reverseString = function(string) {
-  if (string === "") {
-    return "";
-  }
-  else {
-    n = string.length;
-    let newString = string[n - 1]
+// const reverseString = function(string) {
+//   if (string === "") {
+//     return "";
+//   }
+//   else {
+//     n = string.length;
+//     let newString = string[n - 1]
   
-    for (let i = (string.length - 2); i >= 0; i--) {
-      newString += string[i];
-    }
-    return newString;
-  }
+//     for (let i = (string.length - 2); i >= 0; i--) {
+//       newString += string[i];
+//     }
+//     return newString;
+//   }
+// };
+
+// // Do not edit below this line
+// module.exports = reverseString;
+
+const reverseString = function(string) {
+  return string.split("").reverse().join("")
 };
 
 // Do not edit below this line
