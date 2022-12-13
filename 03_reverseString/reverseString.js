f98f6a3c7c081cfd5a4d6d995d43d64e19564c5d
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-13 15:58:19 +0000

    Updated syntax

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,24 +1,24 @@
-// const reverseString = function(string) {
-//   if (string === "") {
-//     return "";
-//   }
-//   else {
-//     n = string.length;
-//     let newString = string[n - 1]
-  
-//     for (let i = (string.length - 2); i >= 0; i--) {
-//       newString += string[i];
-//     }
-//     return newString;
-//   }
-// };
-
-// // Do not edit below this line
-// module.exports = reverseString;
-
 const reverseString = function(string) {
-  return string.split("").reverse().join("")
+  if (string === "") {
+    return "";
+  }
+  else {
+    n = string.length;
+    let newString = string[n - 1]
+  
+    for (let i = (string.length - 2); i >= 0; i--) {
+      newString += string[i];
+    }
+    return newString;
+  }
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// const reverseString = function(string) {
+//   return string.split("").reverse().join("")
+// };
+
+// // Do not edit below this line
+// module.exports = reverseString;
