304c075e448bb864366790bf9171f9d3f7dd2e93
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-13 19:01:42 +0000

    Adds alternative solutions in comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -16,3 +16,21 @@ const removeFromArray = function() {
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// const removeFromArray = function (...args) {
+//   const array = args[0];
+//   const newArray = [];
+//   array.forEach((item) => {
+//     if (!args.includes(item)) {
+//       newArray.push(item);
+//     }
+//   });
+//   return newArray;
+// };
+
+
+// var removeFromArray = function(...args) {
+//   const array = args[0]
+//   return array.filter(val => !args.includes(val))
+// }
