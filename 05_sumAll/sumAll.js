1ba2fb1c415ba283933d6e51038fb8f23edb96f6
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-14 10:11:42 +0000

    Solves Task 5 with simple while loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (a >= 0 && Number.isInteger(a) && b >= 0 && Number.isInteger(b)) {
+    let total = 0;
+    let x = Math.min(a, b);
+    let y = Math.max(a, b);
+    while (x <= y) {
+      total += x;
+      x += 1;
+    }
+    return total;
+  }
+  else {
+    return "ERROR"
+  }
 };
 
 // Do not edit below this line
