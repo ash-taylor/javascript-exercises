0a29da4c78361a6f992034b5783f60b45304efeb
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-14 10:26:05 +0000

    Solves Task 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (year % 4 !== 0 || (year % 100 === 0 && year % 400 !== 0)) return false;
+  else return true;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/* 
+const leapYears = function(year) {
+  return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+};
+*/
