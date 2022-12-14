b32f1f444be40cd84db62a3e22ed4232dfd89a2b
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-14 10:40:01 +0000

    Fixes minor syntax error

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(f) {
-  return Math.round((f - 32) * (5 / 9) * 10) / 10
+  return Math.round((f - 32) * (5 / 9) * 10) / 10;
 };
 
 const convertToFahrenheit = function(c) {
-  return Math.round((c * (9/5) + 32) * 10) / 10
+  return Math.round((c * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
