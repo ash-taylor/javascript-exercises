5f4673fdd1cb2ef87a78855c596db5907ff71f24
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-28 17:52:15 +0000

    Completes all calculator functions and updates tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,30 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = array => array.reduce((total, item) => total + item, 0);
 
-const multiply = function() {
+const multiply = array => array.reduce((total, item) => total * item);
 
-};
+const power = (a, b) => a ** b;
 
-const power = function() {
-	
-};
+const factorial = n => n === 0 ? 1 : n * factorial(n-1);
 
-const factorial = function() {
-	
-};
+// const multiply = function(array) {
+//   return array.length ? array.reduce((accumulator, nextItem) => accumulator * nextItem) : 0;
+// };
+// alternative solution - checks whether there is an array, then runs reduce func or returns 0
+
+// const multiply = function(array) {
+//   return array.reduce(function(total, item) {
+//     return total * item;
+//   }, 1);
+// };
+
+// const factorial = function(n) {
+//   if (n === 0) return 1;
+//   return n * factorial(n-1);
+// };
 
 // Do not edit below this line
 module.exports = {
